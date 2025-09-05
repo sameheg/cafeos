9bbd01afb78138567d2f63748f91b1ffd35b0465
sameheg <webyhost@gmail.com>
2025-09-05 23:26:25 +0300

    feat: add integrations module with ecommerce sync

---
+++ Modules/Integrations/Adapters/WooCommerceAdapter.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Modules\Integrations\Adapters;
+
+class WooCommerceAdapter
+{
+    public function syncOrders(): void
+    {
+        // Logic to sync orders from WooCommerce
+    }
+}
---
+++ Modules/Integrations/Jobs/SyncShopifyOrdersJob.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Modules\Integrations\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Modules\Integrations\Adapters\ShopifyAdapter;
+
+class SyncShopifyOrdersJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public function handle(ShopifyAdapter $adapter): void
+    {
+        $adapter->syncOrders();
+    }
+}
