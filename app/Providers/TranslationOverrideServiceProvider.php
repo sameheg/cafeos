f245f78a342dc07cd919c9a928eba96881095c69
sameheg <webyhost@gmail.com>
2025-09-06 00:48:34 +0300

    fix: guard translation overrides table in tests

--- app/Providers/TranslationOverrideServiceProvider.php
+++ app/Providers/TranslationOverrideServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\TranslationOverride;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class TranslationOverrideServiceProvider extends ServiceProvider
@@ -10,6 +11,10 @@ class TranslationOverrideServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->app->booted(function () {
+            if (! Schema::hasTable('translation_overrides')) {
+                return;
+            }
+
             $locale = app()->getLocale();
             $overrides = TranslationOverride::where('locale', $locale)
                 ->pluck('value', 'key')
