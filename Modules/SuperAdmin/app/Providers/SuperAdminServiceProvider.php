0a0f250c6ea56001058b84fb6b9126e054eb6203
sameheg <webyhost@gmail.com>
2025-09-09 19:28:12 +0300

    Add SuperAdmin module with subscription migrations and tenant interfaces

---
+++ Modules/SuperAdmin/app/Providers/SuperAdminServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\SuperAdmin\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class SuperAdminServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+    }
+
+    public function boot(): void
+    {
+    }
+}
