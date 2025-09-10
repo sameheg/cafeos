fd52cc5f1164d0c62bbd1c84859a950031568f0e
sameheg <webyhost@gmail.com>
2025-09-10 18:29:26 +0300

    test: load module migrations for tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,10 +5,23 @@ namespace Tests;
 use App\Models\Tenant;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Nwidart\Modules\Facades\Module;
 
 abstract class TestCase extends BaseTestCase
 {
-    use RefreshDatabase;
+    use RefreshDatabase {
+        refreshDatabase as baseRefreshDatabase;
+    }
+
+    protected function refreshDatabase()
+    {
+        foreach (Module::allEnabled() as $module) {
+            $module->register();
+            $module->boot();
+        }
+
+        $this->baseRefreshDatabase();
+    }
 
     protected function setUp(): void
     {
