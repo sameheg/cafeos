e4f66d4adb38f394df0559f45561d074bcfc32f4
sameheg <webyhost@gmail.com>
2025-09-11 02:18:47 +0300

    Run central migrations before seeding tenants

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,7 +5,9 @@ namespace Database\Seeders;
 use App\Models\Tenant;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 use Stancl\Tenancy\Contracts\Tenant as TenantContract;
 
 class DatabaseSeeder extends Seeder
@@ -15,6 +17,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        if (! Schema::hasTable('tenants')) {
+            Artisan::call('migrate', [
+                '--path' => database_path('migrations/central'),
+                '--realpath' => true,
+                '--force' => true,
+            ]);
+        }
+
         DB::table('tenants')->insert([
             'id' => 1,
             'name' => 'Test Tenant',
