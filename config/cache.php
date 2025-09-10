f187dfe3852503a42b01bd01bd8a3ea69cb1d091
sameheg <webyhost@gmail.com>
2025-09-11 00:16:52 +0300

    Use array cache in tests

--- config/cache.php
+++ config/cache.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('CACHE_STORE', 'object'),
+    'default' => env('CACHE_STORE', env('APP_ENV') === 'testing' ? 'array' : 'object'),
 
     /*
     |--------------------------------------------------------------------------
