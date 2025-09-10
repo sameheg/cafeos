7433747398bd1cd4a9cab0fb87ea72b7ef6f5fc3
sameheg <webyhost@gmail.com>
2025-09-10 14:40:31 +0300

    chore: enable modules in tests

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,11 +3,15 @@
 use App\Http\Middleware\EnsureModuleEnabled;
 use App\Http\Middleware\InitializeTenancyByDomain;
 use App\Http\Middleware\SetUserLocale;
+use App\Providers\ModuleServiceProvider;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
+    ->withProviders([
+        ModuleServiceProvider::class,
+    ])
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,10 +3,13 @@
 namespace Tests;
 
 use App\Models\Tenant;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
+    use RefreshDatabase;
+
     protected function setUp(): void
     {
         parent::setUp();
