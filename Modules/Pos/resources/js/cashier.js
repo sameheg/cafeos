90f61a3a8c65b24717aaeb9a737d82c9caae0857
sameheg <webyhost@gmail.com>
2025-09-13 07:07:50 +0300

    feat: safeguard cashier shortcuts

--- Modules/Pos/resources/js/cashier.js
+++ Modules/Pos/resources/js/cashier.js
@@ -2,11 +2,19 @@ document.addEventListener('alpine:init', () => {
     Alpine.data('cashierShortcuts', () => ({
         register() {
             window.addEventListener('keydown', (event) => {
+                if (event.repeat) {
+                    return;
+                }
+
                 if (event.key === 'F2') {
+                    event.preventDefault();
+                    event.stopPropagation();
                     this.$dispatch('cashier-checkout');
                 }
 
                 if (event.key === 'F4') {
+                    event.preventDefault();
+                    event.stopPropagation();
                     this.$dispatch('cashier-clear-cart');
                 }
             });
